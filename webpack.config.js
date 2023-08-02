9441d46f18c4453c82f5aaf9f9d5d0fb940eb4dd
Mahmoud <mahmoud.talass@gmail.com>
2023-08-02 12:43:08 -0400

    Add all necessary files and packages for project

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+
+  entry: "./src/index.js",
+
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+};
