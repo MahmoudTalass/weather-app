b50dd26d3a0e7c57b66b40edb2ccf2d986bb7ad9
Mahmoud <mahmoud.talass@gmail.com>
2023-08-04 17:06:27 -0400

    Create functions to insert day date and time to DOM

---
+++ src/scripts/view/weather-view.js
@@ -0,0 +1,14 @@
+function displayTime(time) {
+    const timeContainer = document.querySelector("#time")
+    timeContainer.textContent = time;
+}
+
+function displayDay(day) {
+    const dayContainer = document.querySelector("#day")
+    dayContainer.textContent = day;
+}
+
+function displayDate(date) {
+    const dateContainer = document.querySelector("#date")
+    dateContainer.textContent = date;
+}
