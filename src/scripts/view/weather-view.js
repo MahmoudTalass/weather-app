7d5263dcfbb8a271e61a70c65bc0c12e9c11d0d4
Mahmoud <mahmoud.talass@gmail.com>
2023-08-04 18:14:23 -0400

    Create functions to display date time city and day

--- src/scripts/view/weather-view.js
+++ src/scripts/view/weather-view.js
@@ -1,14 +1,19 @@
 function displayTime(time) {
-    const timeContainer = document.querySelector("#time")
-    timeContainer.textContent = time;
+  const timeContainer = document.querySelector("#time");
+  timeContainer.textContent = time;
 }
 
 function displayDay(day) {
-    const dayContainer = document.querySelector("#day")
-    dayContainer.textContent = day;
+  const dayContainer = document.querySelector("#day");
+  dayContainer.textContent = day;
 }
 
 function displayDate(date) {
-    const dateContainer = document.querySelector("#date")
-    dateContainer.textContent = date;
+  const dateContainer = document.querySelector("#date");
+  dateContainer.textContent = date;
+}
+
+function displayCity(city) {
+    const cityContainer = document.querySelector("#city")
+    cityContainer.textContent = city;
 }
