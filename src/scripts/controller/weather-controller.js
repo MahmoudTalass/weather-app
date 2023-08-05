21888bce8bd5657f1f6c18d0373d53b1874eceec
Mahmoud <mahmoud.talass@gmail.com>
2023-08-05 16:58:11 -0400

    Create function to display day name on screen
    use then on the promise returned by getDayName to extract the result

--- src/scripts/controller/weather-controller.js
+++ src/scripts/controller/weather-controller.js
@@ -1,5 +1,5 @@
-import { getDate, getTime } from "../model/weather-model";
-import { displayDate, displayTime } from "../view/weather-view";
+import { getDate, getDayName, getTime } from "../model/weather-model";
+import { displayDate, displayTime, displayDay } from "../view/weather-view";
 
 function updateTime() {
   const time = getTime();
@@ -11,4 +11,9 @@ function updateDate() {
     date.then((result) => displayDate(result));
 }
 
-export { updateTime, updateDate };
+function updateDay() {
+    const dayName = getDayName()
+    dayName.then((result) => displayDay(result))
+}
+
+export { updateTime, updateDate, updateDay };
