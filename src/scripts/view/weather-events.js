ff4033f6f26a772e269816b3bb3f0397e59b5c39
Mahmoud <mahmoud.talass@gmail.com>
2023-08-09 13:43:40 -0400

    Listen to input on location search and update location options

--- src/scripts/view/weather-events.js
+++ src/scripts/view/weather-events.js
@@ -1,13 +1,19 @@
-import {
-  updateTempScale,
-} from "../controller/weather-controller";
+import { updateLocationOptions, updateTempScale } from "../controller/weather-controller";
 
 function handleWeatherEvents() {
   const tempScale = document.querySelector("#temp-scale");
+  const locationSearch = document.querySelector("#location-search");
+  const locationOptionsContainer = document.querySelector("#location-options")
 
   tempScale.addEventListener("click", () => {
     updateTempScale();
   });
+
+  locationSearch.addEventListener("input", (e) => {
+    const val = e.target.value;
+    if (val.length > 0) updateLocationOptions(val);
+  });
+
 }
 
 export default handleWeatherEvents;
