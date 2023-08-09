2bf5f2e01f6af8f971763f243d2b34c73e65e299
Mahmoud <mahmoud.talass@gmail.com>
2023-08-09 13:50:38 -0400

    Increase the height of location options container if location found

--- src/scripts/view/weather-events.js
+++ src/scripts/view/weather-events.js
@@ -1,9 +1,12 @@
-import { updateLocationOptions, updateTempScale } from "../controller/weather-controller";
+import {
+  updateLocationOptions,
+  updateTempScale,
+} from "../controller/weather-controller";
 
 function handleWeatherEvents() {
   const tempScale = document.querySelector("#temp-scale");
   const locationSearch = document.querySelector("#location-search");
-  const locationOptionsContainer = document.querySelector("#location-options")
+  const locationOptionsContainer = document.querySelector("#location-options");
 
   tempScale.addEventListener("click", () => {
     updateTempScale();
@@ -11,9 +14,13 @@ function handleWeatherEvents() {
 
   locationSearch.addEventListener("input", (e) => {
     const val = e.target.value;
-    if (val.length > 0) updateLocationOptions(val);
+    if (val.length > 0) {
+      locationOptionsContainer.style.height = "6.5rem"
+      updateLocationOptions(val);
+    } else {
+      locationOptionsContainer.style.height = "0"
+    }
   });
-
 }
 
 export default handleWeatherEvents;
