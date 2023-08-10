35930c6fc16559a3652e2ea034d1d0aea22cf6c7
Mahmoud <mahmoud.talass@gmail.com>
2023-08-10 18:29:55 -0400

    Remove unneeded imports

--- src/index.js
+++ src/index.js
@@ -2,39 +2,16 @@
 import reset from "./CSS/reset.css";
 import generalStyle from "./CSS/general-style.css";
 
+import { setCurrentLocation } from "./scripts/model/weather-model";
 import {
-  getForecastInfo,
-  getDate,
-  getDayName,
-  getTime,
-  getLocation,
-  setCurrentLocation,
-} from "./scripts/model/weather-model";
-import {
-  updateTime,
-  updateDate,
-  updateDay,
-  updateCity,
-  updateRegion,
-  updateCountry,
-  updateCurrentTemp,
-  updateTempScale,
-  showTempScale,
-  updateWeatherCondition,
-  updateWeatherConditionIcon,
-  updateTempFeel,
-  updateHumidity,
-  updateFutureWeather,
-  updateLocationOptions,
   startProgram,
   setTimeIntervals,
 } from "./scripts/controller/weather-controller";
 import handleWeatherEvents from "./scripts/view/weather-events";
 
-const localLocation = localStorage.getItem("location")
-if (localLocation === undefined) setCurrentLocation("New York")
+const localLocation = localStorage.getItem("location");
+if (localLocation === undefined) setCurrentLocation("New York");
 
 startProgram();
 handleWeatherEvents();
 setTimeIntervals();
-
